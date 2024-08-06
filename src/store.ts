09c6920f587f6d124d053482b7600830af1813a1
Szachik Sabizański <3525759+kimymt@users.noreply.github.com>
2024-08-06 19:33:19 +0900

    Add files via upload
    Overwriting

--- src/store.ts
+++ src/store.ts
@@ -3,29 +3,10 @@ import { ref } from "vue";
 import * as Nostr from "nostr-tools";
 import { RelayPool } from "nostr-relaypool";
 
-export const feedRelays = ["wss://relay-jp.nostr.wirednet.jp/"];
+export const feedRelays = ["wss://relay.mymt.casa"];
 
 export const profileRelays = [
-  "wss://bostr.nokotaro.com/",
-  "wss://ipv6.nostr.wirednet.jp/",
-  "wss://nos.lol/",
-  "wss://nostr-pub.wellorder.net/",
-  "wss://nostr-relay.nokotaro.com/",
-  "wss://nostr.fediverse.jp",
-  "wss://nostr.holybea.com/",
-  "wss://nostream.ocha.one/",
-  "wss://nrelay-jp.c-stellar.net",
-  "wss://nrelay.c-stellar.net",
-  "wss://offchain.pub/",
-  "wss://purplepag.es/",
-  "wss://r.kojira.io/",
-  "wss://relay-jp.nostr.wirednet.jp/",
-  "wss://relay-jp.shino3.net/",
-  "wss://relay.damus.io/",
-  "wss://relay.nostr.band/",
-  "wss://relay.nostr.wirednet.jp/",
-  "wss://relay.snort.social/",
-  "wss://yabu.me/",
+  "wss://relay.mymt.casa",
 ];
 
 export const pool = new RelayPool(normalizeUrls(feedRelays), {
